2005b2cf1f7029e332e4bba0476f7837416396d6
elithecomputerguy <60199254+elithecomputerguy@users.noreply.github.com>
2024-07-12 11:41:31 -0400

    Delete lab-note.py

--- lab-note.py
+++
@@ -1,9 +0,0 @@
-filename = 'note.html'
-
-with open(filename, 'w') as file:
-    file.write('<h1>Note App</h1>\n')
-
-while True:
-    update = input('Add an Update: ')
-    with open(filename, 'a') as file:
-        file.write(f'<p>{update}</p>\n')
